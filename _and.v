d8ac3e89ced60a104e1435e4a1a83f6db9fde1c4
sibilina98 <sibilina.julian0381@alumnos.udg.mx>
2025-08-18 12:52:50 -0600

    compuerta and

---
+++ _and.v
@@ -0,0 +1,10 @@
+//1.Definicion del modulo y sus entradas y salidas
+module _and(input A, input B, output C );
+//2. Declarar señales/elementos internos
+//NA
+//3.Comportamiento del modulo
+//  (asifnaciones, instanicas, conexiones)
+assign C = A&B; 
+
+endmodule
+//https://github.com/sibilina98/ArquitecturaComputadoras.git 
